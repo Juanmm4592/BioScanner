7d67ded27508144232f94a2bde81b51b9c7d4e8c
Juan Manuel <juanmm4592@gmail.com>
2020-07-25 19:07:02 -0500

    Agregar archivos de proyecto.

---
+++ BioScanner/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace BioScanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvEnfermedades = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEnfermedades)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEnfermedades
+            // 
+            this.dgvEnfermedades.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEnfermedades.Location = new System.Drawing.Point(12, 96);
+            this.dgvEnfermedades.Name = "dgvEnfermedades";
+            this.dgvEnfermedades.Size = new System.Drawing.Size(776, 342);
+            this.dgvEnfermedades.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvEnfermedades);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEnfermedades)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEnfermedades;
+    }
+}
+
---
+++ BioScanner/SQL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BioScanner
+{
+    public class SQL
+    {
+        private static SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["cnn"].ConnectionString);
+
+        // Metodo para llenar un GridView
+        public static void ListarConsultaSQL(string consulta, DataGridView grilla)
+        {
+            SqlCommand cmd = new SqlCommand(consulta, cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            DataTable tabla = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            sda.Fill(tabla);
+            grilla.DataSource = tabla;
+        }
+    }
+}
